85bda910bea5bbf861baf2ee2cc1da9e8fca7ae8
michaeljdarcy <180347967+michaeljdarcy-usa@users.noreply.github.com>
2025-06-07 19:22:16 -0400

    Addedd code to prompt user for their full name

--- chap5p7quiz-question1.cpp
+++ chap5p7quiz-question1.cpp
@@ -1,3 +1,15 @@
 /* Write a program that asks the user to enter their full name and their age. As output, tell the user the sum of their age and the number of characters in their name (use the std::string::length() member function to get the length of the string). For simplicity, count any spaces in the name as a character. */
 
+#include <iostream>
+#include <string>
 
+int main(int argc, char * argv[])
+{
+    std::cout << "Enter your full name: " << std::endl;
+    std::string name{};
+    std::getline(std::cin >> std::ws, name);
+
+    std::cout << name;
+
+    return 0;
+}
