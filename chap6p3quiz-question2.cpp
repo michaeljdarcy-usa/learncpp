51225611fda409b15718ef010b629c8df8025cfd
michaeljdarcy <180347967+michaeljdarcy-usa@users.noreply.github.com>
2025-06-28 09:13:58 -0400

    writing program that tells user input if number is even or odd

---
+++ chap6p3quiz-question2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+// Write a constexpr function called isEven() that returns true if an integer passed to it is even, and false otherwise.
+constexpr int isEven()
+{
+    
+}
+
+int main(int argc, char* argv[])
+{
+    std::cout << "Enter an integer: " << std::endl;
+
+    return 0;
+}
