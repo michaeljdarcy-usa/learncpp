39dad469c7bbe75f6e235f32b9cd0012ca5ce6b2
michaeljdarcy <180347967+michaeljdarcy-usa@users.noreply.github.com>
2025-06-28 09:42:57 -0400

    added remaining logic

--- chap6p3quiz-question2.cpp
+++ chap6p3quiz-question2.cpp
@@ -1,14 +1,32 @@
 #include <iostream>
 
 // Write a constexpr function called isEven() that returns true if an integer passed to it is even, and false otherwise.
-constexpr int isEven()
+constexpr int isEven(int x)
 {
-    
+    if ((x % 2) == 0)
+    {
+        return true;
+    }
+    else
+    {
+        return false;
+    }
 }
 
 int main(int argc, char* argv[])
 {
     std::cout << "Enter an integer: " << std::endl;
+    int num{};
+    std::cin >> num;
+
+    if (isEven(num))
+    {
+        std::cout << "The number is even.\n";
+    }
+    else
+    {
+        std::cout << "The number is odd.\n";
+    }
 
     return 0;
 }
